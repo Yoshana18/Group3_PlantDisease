c3a3739d3def709b4e187acb857f2e078e907b9f
Yoshana18 <yoshana.magendran.18@gmail.com>
2025-01-25 14:45:08 +0800

    did the outlook of app

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 welcome to our cv project
+
+This is our plant disease detection system. 
+
+Model used here is CNN
--- app/models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-from datetime import datetime
-from decimal import Decimal
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-#
-# class Plant(db.Model):
