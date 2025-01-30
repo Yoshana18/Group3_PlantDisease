cbc51f23a83d12ac57c3f293c313f2835a48472d
Yoshana18 <yoshana.magendran.18@gmail.com>
2025-01-30 09:02:59 +0800

    organised and changed a lot that gave me a heartattack after it stopped working

--- app.py
+++ app.py
@@ -1,24 +1,8 @@
-# disease_mapping = {
-#     'Tomato_healthy', 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato_Septoria_leaf_spot', 'Tomato_Late_blight',
-#     'Tomato_Early_blight', 'Tomato_Bacterial_spot', 'Tomato_Leaf_Mold', 'Tomato_Target_Spot',
-#     'Tomato_Tomato_mosaic_virus', 'Potato_healthy', 'Potato_Late_blight', 'Potato_Early_blight',
-#     'Grape_Black_rot', 'Grape_healthy', 'Grape_Esca_(Black_Measles)', 'Grape_Leaf_blight_(Isariopsis_Leaf_Spot)',
-#     'Pepper_bell_Bacterial_spot', 'Pepper_bell_healthy', }
-# #
-#
-# @app.route('/predict', methods=['GET', 'POST'])
-# def predict():
-#     if request.method == 'POST':
-#         image = request.files['image']
-#         filename = image.filename
-#         file_path = os.path.join('static/uploads', filename)
-#         image.save(file_path)
-#         print(file_path)
-#         return render_template('index.html')
+from flask import Flask, request, redirect, url_for, render_template
+from application import templates
+import os
 
-# import flask modules
-from flask import Flask, render_template, url_for
-
+# from pyparsing.diagram import template
 
 app = Flask(__name__)
 
@@ -27,5 +11,14 @@ def index():
     # Main page
     return render_template('index.html') #render home page
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+
+# @app.route('/home', methods=['GET', 'POST'])
+# def home():
+#     if request.method == 'POST':
+#         file = request.files['image']
+#         if file:
+#             filename = os.path.join('static/uploads', file.filename)
+#             file.save(filename)
+#             # Perform prediction logic here (e.g., call ML model)
+#             return redirect(url_for('home'))
+#     return render_template('index.html')
